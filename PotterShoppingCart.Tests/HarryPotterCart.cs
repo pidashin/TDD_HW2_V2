f964f7355792d4fed690f05a30a9ffc9b4fea039
pidashin <pidashin00003@yahoo.com.tw>
2015-09-23 20:18:30 +0800

    update prod code

--- PotterShoppingCart.Tests/HarryPotterCart.cs
+++ PotterShoppingCart.Tests/HarryPotterCart.cs
@@ -10,8 +10,22 @@ namespace PotterShoppingCart.Tests
         public double CountPrice(List<Book> books)
         {
             var discounts = new List<double> { 1, 1, 0.95, 0.9, 0.8, 0.75};
-            var discount = discounts[books.Count()];
-            return books.Sum(item => item.price * discount);
+
+            var source = books.GroupBy(item => item.Episode).Select(item => new { Count = item.Count(), Price = item.ToList()[0].price});
+
+            var groupCount = source.Select(item => item.Count).Distinct().OrderBy(item => item);
+
+            double result = 0;
+            int countSum = 0;
+            groupCount.ToList().ForEach(c =>
+            {
+                source = source.Where(temp => temp.Count >= c);
+                var discount = discounts[source.Count()];
+                result += source.Sum(temp => temp.Price * discount * (c - countSum));
+                countSum = c;
+            });
+
+            return result;
         }
     }
 }
