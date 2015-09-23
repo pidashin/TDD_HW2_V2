eacf4498657658627ef5deb2acc8ca915654cf64
pidashin <pidashin00003@yahoo.com.tw>
2015-09-23 19:40:12 +0800

    add book class

---
+++ PotterShoppingCart.Tests/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart.Tests
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Serial { get; set; }
+        public int Episode { get; set; }
+        public int price { get; set; }
+    }
+}
