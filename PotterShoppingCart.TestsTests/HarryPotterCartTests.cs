17b8de4d7742b27cfa32ab2ca801feb99ccd0326
pidashin <pidashin00003@yahoo.com.tw>
2015-09-23 19:47:19 +0800

    create test scenario1 第一集買了一本_價格應100元

--- PotterShoppingCart.TestsTests/HarryPotterCartTests.cs
+++ PotterShoppingCart.TestsTests/HarryPotterCartTests.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace PotterShoppingCart.TestsTests
+using PotterShoppingCart.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace PotterShoppingCart.Tests.Tests
 {
-    class HarryPotterCartTests
+    [TestClass()]
+    public class HarryPotterCartTests
     {
+        [TestMethod()]
+        public void 第一集買了一本_價格應100元()
+        {
+            //Arrange
+            var books = new List<Book> { new Book { Serial = "HarryPotter", Episode = 1, price = 100 } };
+            var target = new HarryPotterCart();
+            var expected = 100;
+
+            //Act
+            var actual = target.CountPrice(books);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
+
+
 }
