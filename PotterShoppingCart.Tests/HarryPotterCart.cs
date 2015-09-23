22387d46e68da7539a773e4c8e5739ac66623f88
pidashin <pidashin00003@yahoo.com.tw>
2015-09-23 19:43:05 +0800

    add HarryPotterCart and generate Unit Test

---
+++ PotterShoppingCart.Tests/HarryPotterCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart.Tests
+{
+    public class HarryPotterCart
+    {
+        public int CountPrice(List<Book> books)
+        {
+            return 0;
+        }
+    }
+}
